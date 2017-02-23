ba263349d028e57baa9d2d81f2a6ccdb55f6a889
jquag <mail@johnquagliata.com>
2017-02-23 07:29:00 -0600

    changes

---
+++ scripts/ngConstants.js
@@ -0,0 +1,11 @@
+angular.module("ngConstants", [])
+
+.constant("envName", "qa")
+
+.constant("secureCookies", true)
+
+.constant("serviceUrl", "https://grace-groups.herokuapp.com")
+
+.constant("partialPath", "partials")
+
+;
